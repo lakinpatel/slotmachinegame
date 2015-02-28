d100fedb7d949982674f50a26f59dd6c1791db77
lakinpatel <lakinpatel@yahoo.com>
2015-02-27 19:51:39 -0500

    Added Functions in TS Files

--- Scripts/game.ts
+++ Scripts/game.ts
@@ -4,20 +4,6 @@
 var canvas;
 var stage: createjs.Stage;
 
-// Game Objects 
-var game: createjs.Container;
-var background: createjs.Bitmap;
-var spinButton: objects.Button;
-var betOneButton: objects.Button;
-var betMaxButton: objects.Button;
-var powerButton: objects.Button;
-var resetButton: objects.Button;
-var tiles: createjs.Bitmap[] = [];
-var reelContainers: createjs.Container[] = [];
-
-// GAME CONSTANTS
-var NUM_REELS: number = 3;
-
 // Game Variables
 var playerMoney = 1000;
 var winnings = 0;
@@ -30,6 +16,22 @@ var spinResult;
 var fruits = "";
 var winRatio = 0;
 
+// Game Objects 
+var game: createjs.Container;
+var background: createjs.Bitmap;
+var spinButton: objects.Button;
+var betOneButton: objects.Button;
+var betMaxButton: objects.Button;
+var powerButton: objects.Button;
+var resetButton: objects.Button;
+var tiles: createjs.Bitmap[] = [];
+var reelContainers: createjs.Container[] = [];
+var currentMoneyText:createjs.Text = new createjs.Text(playerMoney.toString(), "23px play", "#000000");
+var currentBetText:createjs.Text= new createjs.Text(playerBet.toString(), "23px play", "#000000");
+var jackpotText:createjs.Text = new createjs.Text(jackpot.toString(), "23px play", "#000000");
+
+// GAME CONSTANTS
+var NUM_REELS: number = 3;
 
 /* Tally Variables */
 var grapes = 0;
@@ -58,6 +60,18 @@ function gameLoop() {
 }
 
 
+// reset all
+function resetAll() {
+    playerMoney = 1000;
+    winnings = 0;
+    jackpot = 5000;
+    turn = 0;
+    playerBet = 0;
+    winNumber = 0;
+    lossNumber = 0;
+    winRatio = 0;
+}
+
 /* Utility function to reset all fruit tallies */
 function resetFruitTally() {
     grapes = 0;
@@ -70,29 +84,71 @@ function resetFruitTally() {
     blanks = 0;
 }
 
-// Event handlers
-
-/*function spinButtonOut() {
-
-    spinButton.alpha = 1.0;
-
-
-}
-
-function spinButtonOver() {
-    spinButton.alpha = 0.5;
-
-}*/
-
-function powerOff() {
+// Event handler for PowerButton
+function powerOff(event) {
     createUI();
 }
 
-function spinReels() {
+
+// Event Handler for Bet One Button
+function betOneButtonClicked(event) {
+    playerBet = 1;
+    if (spinReels) {
+        playerBet--;
+    }
+}
+
+// Event handler for BetMaxButton
+function betMaxButtonClicked() {
+    console.log("Bet Max Clicked");    
+}
+
+
+// Event Handler for ResetButton
+function resetButtonClicked(event) {
+    createUI();
+}
+
+// Event Handler for SpinButton
+function spinButtonClicked(event) {
+    if (playerMoney == 0) {
+        if (confirm("You ran out of Money! \nDo you want to play again?")) {
+            resetAll();
+        }
+    }
+    else if (playerBet > playerMoney) {
+        alert("You don't have enough Money to place that bet.");
+    }
+    else if (playerBet < 0) {
+        alert("All bets must be a positive $ amount.");
+    }
+    else if (playerBet <= playerMoney) {
+        spinResult = spinReels(event);
+        fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+        determineWinnings();
+        turn++;
+    }
+    else {
+        alert("Please enter a valid bet amount");
+    }
+
+    currentMoneyText.text = playerMoney.toString();
+    currentBetText.text = playerBet.toString();
+    jackpotText.text = jackpot.toString();
+
+    game.addChild(currentMoneyText);
+    //game.addChild(jackpot);
+    game.addChild(currentBetText);
+}
+
+
+
+// Event Handler for Spin Button
+function spinReels(event) {
     // Add Spin Reels code here
     spinResult = Reels();
     fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
-    console.log(fruits);
+    //console.log(fruits);
 
 
     for (var tile = 0; tile < 3; tile++) {
@@ -100,10 +156,9 @@ function spinReels() {
             game.removeChild(tiles[tile]);
         }
         tiles[tile] = new createjs.Bitmap("assets/images/" + spinResult[tile] + ".png");
-        //tiles[tile].x = 59 + (75 * tile);
-        //tiles[tile].y = 188;
+        
         game.addChild(tiles[tile]);
-        console.log(game.getNumChildren());
+        //console.log(game.getNumChildren());
     }
     tiles[0].x = 105;
     tiles[0].y = 194;
@@ -111,6 +166,8 @@ function spinReels() {
     tiles[1].y = 194;
     tiles[2].x = 205;
     tiles[2].y = 194;
+    
+    return spinResult;
 
 }
 
@@ -224,57 +281,85 @@ function determineWinnings() {
             winnings = playerBet * 5;
         }
         winNumber++;
-       // showWinMessage();
+        showWinMessage();
     }
     else {
         lossNumber++;
-      //  showLossMessage();
+        showLossMessage();
     }
 
 }
 
+/* Check to see if the player won the jackpot */
+function checkJackPot() {
+    /* compare two random values */
+    var jackPotTry = Math.floor(Math.random() * 51 + 1);
+    var jackPotWin = Math.floor(Math.random() * 51 + 1);
+    if (jackPotTry == jackPotWin) {
+        alert("You Won the $" + jackpot + " Jackpot!!");
+        playerMoney += jackpot;
+        jackpot = 1000;
+    }
+}
+
+function showWinMessage() {
+    playerMoney += winnings;
+    resetFruitTally();
+    checkJackPot();
+
+}
+
+function showLossMessage() {
+    playerMoney -= playerBet;
+    resetFruitTally();
+}
+
 function createUI():void {
-    // instantiate my background
-    background = new createjs.Bitmap("assets/images/red_slot-machinejpg.jpg");
+    // instantiate the Main Slot Machine Image
+    background = new createjs.Bitmap("assets/images/red_slot-machinejpg.jpg"); 
     game.addChild(background);
 
     for (var index = 0; index < NUM_REELS; index++) {
-        //reelContainers[index].removeChild();
         reelContainers[index] = new createjs.Container();
         game.addChild(reelContainers[index]);
     }
-    reelContainers[0].x = 108;
-    reelContainers[0].y = 196;
-    reelContainers[1].x = 158;
-    reelContainers[1].y = 196;
-    reelContainers[2].x = 210;
-    reelContainers[2].y = 196;
+
+    // Adding Reels to 3 of the Containers with their X & Y coordinates
+    reelContainers[0].x = 105;
+    reelContainers[0].y = 194;
+    reelContainers[1].x = 155;
+    reelContainers[1].y = 194;
+    reelContainers[2].x = 205;
+    reelContainers[2].y = 194;
 
     // Spin Button
     spinButton = new objects.Button("assets/images/spinButton1.png", 225, 306);
-    game.addChild(spinButton.getImage());
-    spinButton.getImage().addEventListener("click", spinReels);
+    game.addChild(spinButton.getImage()); // getting Image of Button and displaying
+    spinButton.getImage().addEventListener("click", spinButtonClicked); // Registering the Click Event for SpinButton
 
     // Bet One Button
     betOneButton = new objects.Button("assets/images/betOne.png", 170, 306);
-    game.addChild(betOneButton.getImage());
+    game.addChild(betOneButton.getImage()); // getting Image of Button and displaying
+    betOneButton.getImage().addEventListener("click", betOneButtonClicked); // Registering the Click Event for BetOne Button
 
 
     // Bet Max Button
     betMaxButton = new objects.Button("assets/images/betMax.png", 170, 326);
-    game.addChild(betMaxButton.getImage());
+    game.addChild(betMaxButton.getImage()); // getting Image of Button and displaying
+    betMaxButton.getImage().addEventListener("click", betMaxButtonClicked); // Registering the Click Event for BetMax button
 
     // Power Button
     powerButton = new objects.Button("assets/images/powerButton.png", 70, 306);
-    game.addChild(powerButton.getImage());
-    powerButton.getImage().addEventListener("click",powerOff);
+    game.addChild(powerButton.getImage()); // getting Image of Button and displaying
+    powerButton.getImage().addEventListener("click", powerOff); // Registering the Click Event for Power Button
 
     // Reset Button
     resetButton = new objects.Button("assets/images/resetButton.png", 125, 306);
-    game.addChild(resetButton.getImage());
-    resetButton.getImage().addEventListener("click", function () {
-        console.log("reset clicked");
-    });
+    game.addChild(resetButton.getImage()); // getting Image of Button and displaying
+    resetButton.getImage().addEventListener("click", resetButtonClicked); // Registering the Click Event for ResetButton
+
+    
+
 }
 
 
@@ -288,7 +373,5 @@ function main() {
 
     // Create Slotmachine User Interface
     createUI();
-
-
     stage.addChild(game);
 }
